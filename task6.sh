ef82f84b5d251b446cbc8ba3b19caf2217237f2b
Joshua Taylor Dean SMANDYCH <smandych@euler-login-2.engr.wisc.edu>
2025-02-02 14:20:11 -0600

    adding files from Q6

---
+++ task6.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=task6
+#SBATCH --partition=instruction
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=0-00:00:10
+#SBATCH --output=task6_%j.out
+#SBATCH --error=task6_%j.err
+
+# Compile the program (ensure it's up-to-date)
+g++ task6.cpp -Wall -O3 -std=c++17 -o task6
+
+srun ./task6 $N
